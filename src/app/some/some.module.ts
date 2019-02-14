eadf4adb4758d84333289e18b12ba3d713a211a8
Eraldo Energy <eraldo@eraldo.org>
2019-02-14 13:27:12 -0500

    Add code to demo issue

---
+++ src/app/some/some.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SomePage } from './some.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SomePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [SomePage]
+})
+export class SomePageModule {}
---
+++ src/app/some/some.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-some',
+  templateUrl: './some.page.html',
+  styleUrls: ['./some.page.scss'],
+})
+export class SomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
